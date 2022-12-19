a4e91225979f77b9e61d9ecf5529498c2c9a97ce
Itoro Ikon <itoroikon2@gmail.com>
2022-12-19 03:43:41 +0100

    Add to repo

---
+++ dan_lev/spiders/better_fyi.py
@@ -0,0 +1,76 @@
+import scrapy
+import pprint
+import re
+from urllib.parse import urlparse
+from dan_lev.items import TrackerItem
+
+class BetterFyiSpider(scrapy.Spider):
+    name = "better_fyi_spider"
+    start_urls =[
+        'https://better.fyi/trackers/',
+        # 'https://better.fyi/trackers/adform.net/',
+        # 'https://better.fyi/trackers/honorableland.com/',
+        # 'https://better.fyi/trackers/1rx.io/'
+    ]
+
+    def parse(self, response):
+        count = 0
+        for tracker in response.xpath("//ul[@data-set-text='html list']/li/a"):
+            tracker_item = TrackerItem()
+            href_val = tracker.xpath("@href").get()
+            tracker_found_url = response.urljoin(href_val)+"/"            
+            tracker_title = tracker.xpath("./strong/text()").get()
+            tracker_website = tracker.xpath("text()").get()
+            tracker_item["found_url"] = tracker_found_url
+            tracker_item["title"] = tracker_title
+            tracker_item["website"] = tracker_website
+            
+            request = scrapy.Request(tracker_found_url,
+                             callback=self.parse_tracker,
+                             cb_kwargs=dict(tracker_item=tracker_item))
+            # request.cb_kwargs['tracker_title'] = tracker_title
+            # request.cb_kwargs['tracker_website'] = tracker_website
+            yield request
+            
+            
+            count += 1
+            print("count: ", count)
+            # if count == 100:
+                # break
+            
+    def parse_tracker(self, response, tracker_item):
+        tracker_description_p = response.xpath("//blockquote/p/text()").get()
+        tracker_description_a = response.xpath("//blockquote/p/a/text()").get()
+        tracker_description = None
+        tracker_description_source = ""
+        href_value = response.xpath("//blockquote/p/a/@href").get()
+        
+        # set parsing rule for tracker_description_source field
+        if tracker_description_a and tracker_description_a.strip() == "Source":
+            tracker_description = tracker_description_p.replace(chr(8221), "").replace(chr(8220), "")[:-3]
+            tracker_description_source = href_value
+        else:
+            if tracker_description_p and tracker_description_a:
+                tracker_description = tracker_description_p + tracker_description_a
+            parsed_url = urlparse(response.url)
+            fully_qualified_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_url)
+            if fully_qualified_domain and href_value:
+                tracker_description_source = fully_qualified_domain + href_value
+            
+        rule_text = response.xpath('//code[@class="mson"]//text()').getall()
+        rule_text = re.sub(' +', ' ', "".join(rule_text)+'\n')
+        bool_val = response.xpath('boolean(//code[@class="mson"]/span)').get()
+        tracker_block_rule = ""
+        
+        # set parsing rule for tracker_block_rule field
+        if int(bool_val):
+            tracker_block_rule = rule_text
+        else:
+            tracker_block_rule = ''.join(rule_text.split('\n')).replace('\n', '')
+        
+        
+        tracker_item['description'] = tracker_description
+        tracker_item['description_source'] = tracker_description_source
+        tracker_item['block_rule'] = tracker_block_rule
+        
+        yield tracker_item
